73a8f4c5632d94618daeeafb0c49ae3f5aeaf4b1
YoonSung Kim <opqudk@gmail.com>
2020-06-08 16:54:35 +0900

    #2.3 Modifying the DOM with JS

--- Practice/index4.js
+++ Practice/index4.js
@@ -1,5 +1,13 @@
 console.log(document);
 
 const title = document.getElementById("title");
+const tit = document.querySelector("#title");
+
 console.log(title);
+
 title.innerHtML = "Hi From JS";
+title.style.color = "red";
+tit.style.backgroundColor = "black";
+console.dir(document);
+
+document.title = "I own you now";
